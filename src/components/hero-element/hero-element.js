63634ab9c3da1246256aed8634381dce171bbc62
Ghilman Al Fatih <ghilman27@gmail.com>
2020-10-12 23:00:45 +0700

    modify implementation of hero image background as a props

--- src/components/hero-element/hero-element.js
+++ src/components/hero-element/hero-element.js
@@ -1,16 +1,24 @@
-import { LitElement, html, customElement} from 'lit-element';
+import { LitElement, html, customElement, property} from 'lit-element';
 import './hero-element.scss';
 import './hero-element_responsive.scss';
 
 @customElement('hero-element')
 export default class HeroElement extends LitElement {
+    @property({type: String})
+    imageSrc = "/images/heros/hero-image_2.jpg";
+
     render() {
         return html`
+        <div class="hero">
+            <div class="hero__background">
+                <img src="${this.imageSrc}"/>
+            </div>
             <div class="hero__placeholder">
                 <p class="hero__greeting" tabindex="0">Good night,</p>
                 <p class="hero__name" tabindex="0">Ghilman</p>
                 <h1 class="hero__desc" tabindex="0">Let's explore good foods near you!</h1>
             </div>
+        </div>
         `;
     }
 
