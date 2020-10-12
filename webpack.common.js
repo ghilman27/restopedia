e812670ba78b74a0ae179113f42a9dd5b1b14ba6
Ghilman Al Fatih <ghilman27@gmail.com>
2020-10-12 18:14:07 +0700

    configure dotenv

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssnanoWebpackPlugin = require('cssnano-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const path = require('path');
 
 module.exports = {
@@ -64,6 +65,7 @@ module.exports = {
     }),
     new CssnanoWebpackPlugin({
       sourceMap: true
-    })
+    }),
+    new Dotenv()
   ],
 };
