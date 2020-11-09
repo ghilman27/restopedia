6c7fe75b47a3675aa40ef3ca0eef45f76e64def3
ghilaman27 <ghilman27@gmail.com>
2020-11-10 01:28:42 +0700

    more complete manifest json, revise worker
    - add more icons and fields to manifest.json
    - add more meta info to index.html
    - implement expiration plugin only to google webfonts cache

--- src/worker.js
+++ src/worker.js
@@ -9,19 +9,18 @@ import { ExpirationPlugin } from 'workbox-expiration';
 skipWaiting();
 clientsClaim();
 
+const DEFAULT_STRATEGY = StaleWhileRevalidate;
+const DEFAULT_PLUGINS = [
+	new CacheableResponsePlugin({
+		statuses: [200],
+	}),
+];
+
 const registerRoute = ({
 	origin,
 	name,
-	strategy = StaleWhileRevalidate,
-	plugins = [
-		new CacheableResponsePlugin({
-			statuses: [200],
-		}),
-		new ExpirationPlugin({
-			maxAgeSeconds: 60 * 60 * 24 * 365,
-			maxEntries: 30,
-		}),
-	],
+	strategy = DEFAULT_STRATEGY,
+	plugins = DEFAULT_PLUGINS,
 }) => {
 	workboxRegisterRoute(
 		({ url }) => url.origin === origin,
@@ -51,28 +50,13 @@ registerRoute({
 	origin: process.env.GOOGLE_WEBFONTS_URL,
 	name: process.env.GOOGLE_WEBFONTS_CACHE_NAME,
 	strategy: CacheFirst,
+	plugins: [
+		...DEFAULT_PLUGINS,
+		new ExpirationPlugin({
+			maxAgeSeconds: 60 * 60 * 24 * 365,
+			maxEntries: 30,
+		}),
+	],
 });
-
-// self.addEventListener('push', function (event) {
-// 	let body;
-// 	if (event.data) {
-// 		body = event.data.text();
-// 	} else {
-// 		body = 'Push message no payload';
-// 	}
-// 	const options = {
-// 		body: body,
-// 		icon: 'images/icon192.png',
-// 		badge: 'images/icon192.png',
-// 		vibrate: [100, 50, 100],
-// 		data: {
-// 			dateOfArrival: Date.now(),
-// 			primaryKey: 1,
-// 		},
-// 	};
-// 	event.waitUntil(
-// 		self.registration.showNotification('Push Notification', options)
-// 	);
-// });
 
 precacheAndRoute(self.__WB_MANIFEST);
