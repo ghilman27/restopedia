28511ef4a912107f569adedc313fce22b7683bd5
ghilaman27 <ghilman27@gmail.com>
2020-11-12 01:01:26 +0700

    add aria label to loading content

--- src/components/loading-indicator/loading-indicator.js
+++ src/components/loading-indicator/loading-indicator.js
@@ -6,13 +6,13 @@ import './loading-indicator.scss';
 @customElement('loading-indicator')
 export default class LoadingIndicator extends LitElement {
     @property({ type: String })
-    loadingText = 'Loading Content ...';
+    loadingText = 'Loading Content';
 
     render() {
         return html`
             <div class="container">
                 <div class="loader"></div>
-                <p>${this.loadingText}</p>
+                <p tabindex="0">${this.loadingText}</p>
             </div>
         `;
     }
